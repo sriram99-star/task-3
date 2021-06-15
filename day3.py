aa87f91e8bbf3651dcec4b9da1a55cecc5657673
sriram99-star <85795394+sriram99-star@users.noreply.github.com>
2021-06-15 20:37:28 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,24 @@
+a = b = c = 50
+
+#Divide a by 10
+divi = a/10
+print(int(divi))
+
+#Multiply b by 50
+mul = b * 50
+print(mul)
+
+#Add c value by 60
+add_1 = c + 60
+print(add_1)
+
+#Create a String variables (a,b) of 5 characters and replace the 3rd character with G
+var_1 = "abcde"
+x = var_1.replace(var_1[2],"G")
+print(x)
+
+#Create two values (a,b) of int,float data type & convert the vise versa
+a,b = 15.5,10.2
+a1 = (float(a))
+b1 = (int(b))
+print(a1,b1)
